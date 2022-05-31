a4e6bdf113431bbd6a9b75d3cfda513f811a4687
Albert Jokelin <albertjokelin@icloud.com>
2022-05-31 12:05:21 +0400

    Added animation

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,11 +5,14 @@ import About from "../components/about"
 import Markets from "../components/markets";
 import Transact from "../components/transact"
 import Wallets from "../components/wallets"
+import { Spline } from 'react-spline';
+
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Spline scene="https://prod.spline.design/nhhYREQ2AgMD9DjP/scene.splinecode" />
       <About />
       <Transact />
       <Markets />
@@ -18,3 +21,4 @@ export default function Home() {
     </div>
   )
 }
+
