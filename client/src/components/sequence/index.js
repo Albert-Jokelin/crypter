46246cc49484c8049bd4d88df12593100ab667e1
Albert Jokelin <albertjokelin@icloud.com>
2022-06-04 13:06:47 +0400

    Sequence done

--- client/src/components/sequence/index.js
+++ client/src/components/sequence/index.js
@@ -1,14 +1,16 @@
-import React from "react";
-import video from ".../../../src/images/video.webm";
+import React from "react"
+import ReactPlayer from 'react-player'
+import video from ".../../../src/images/video.webm"
+import bg from ".../../../src/images/bg.jpg"
 
 export default function Sequence() {
   return (
-    <div className="container">
-      <div className="video-wrapper">
-        <video autoplay muted>
-          <source src={video} />
-        </video>
-      </div>
+    <div className="container" style={{
+      backgroundImage: `url(${bg})`,
+      backgroundRepeat: `no-repeat`,
+      height: `100vh`,
+      backgroundSize: `100% 100%`}}>
+      <ReactPlayer url={video} muted={true} playing={true} loop={true} width='100vw' height='100vh'/>
     </div >
   );
 }
